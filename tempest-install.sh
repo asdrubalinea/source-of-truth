2aef8979ade704ea6715687ca88269818ffcb0e8
Irene <git@asdrubalini.xyz>
2025-05-01 16:09:52 +0000

    tempest

--- tempest-install.sh
+++ tempest-install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo nix --extra-experimental-features nix-command --extra-experimental-features flakes run 'github:nix-community/disko/latest#disko-install' -- --flake '.#tempest' --disk main /dev/nvme0n1
+sudo nix --extra-experimental-features nix-command --extra-experimental-features flakes run 'github:nix-community/disko/latest#disko-install' -- --flake '.#tempest' --write-efi-boot-entries --disk main /dev/nvme0n1
