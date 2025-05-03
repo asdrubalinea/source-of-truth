2f60d4d0d6c34815ce069615572eef3c2504f839
Irene <git@asdrubalini.xyz>
2025-05-03 19:33:39 +0200

    revert flake.nix

--- flake.nix
+++ flake.nix
@@ -184,6 +184,17 @@
 
             ./disks/tempest.nix
             ./hosts/tempest.nix
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.extraSpecialArgs = {
+                inherit inputs;
+                hostname = "tempest";
+              };
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.irene = import ./homes/tempest.nix;
+            }
           ];
         };
       };
