cef5ccfb9d4e227f694103eb65fc8f2a66d91b19
Irene <git@asdrubalini.xyz>
2025-08-02 14:14:35 +0200

    nix

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,29 +1,34 @@
 { pkgs, lib, config, ... }:
 {
-  # Shared Nix configuration across all hosts
   nix = {
     package = pkgs.nixVersions.stable;
 
     settings = {
-      # Base trusted users - hosts can extend this list
       trusted-users = [
         "root"
         "irene"
       ];
 
-      # Common substituters for performance
-      substituters = [
-        "https://cache.nixos.org/"
-        "https://hyprland.cachix.org"
-        "https://cosmic.cachix.org/"
-      ];
+      substituters = 
+        if config.networking.hostName == "tempest" then [
+          "http://orchid.boreal-city.ts.net:8501/"
+        ]
+        else [
+          "https://cache.nixos.org/"
+          "https://hyprland.cachix.org"
+          "https://cosmic.cachix.org/"
+        ];
 
       # Corresponding public keys
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
-      ];
+      trusted-public-keys = 
+        if config.networking.hostName == "tempest" then [
+          "orchid:OonqQD3i5uEEi8h3zSxxp/uvVGR+Mum0/mbJohLJ09I="
+        ]
+        else [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+        ];
 
       # Performance optimizations
       max-jobs = lib.mkDefault "auto";
@@ -55,5 +60,4 @@
       keep-derivations = true
     '';
   };
-
 }
