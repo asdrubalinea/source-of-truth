1cb939374b2c89b56009c968b0953baf552d3072
Irene <git@irene.foo>
2025-10-27 21:00:47 +0100

    updates

--- desktop/zed-editor/default.nix
+++ desktop/zed-editor/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
-  home.packages = [ pkgs.trunk.zed-editor ];
+  home.packages = [ pkgs.zed-editor ];
   # home.file.".config/zed/settings.json".source = ./settings.jsonc;
   # home.file.".config/zed/keymap.json".source = ./keymap.jsonc;
 }
