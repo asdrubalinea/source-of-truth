fb0db6e7c8f957feda3e65ae2a9dc7ef94e21162
Irene <asdrubalini@mail.com>
2025-12-06 01:46:48 +0100

    caddy

---
+++ services/caddy/default.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+let
+  config = builtins.readFile ./Caddyfile;
+in
+{
+  services.caddy = {
+    enable = true;
+
+    package = pkgs.caddy.withPlugins {
+      plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
+      hash = "sha256-ea8PC/+SlPRdEVVF/I3c1CBprlVp1nrumKM5cMwJJ3U=";
+    };
+
+    globalConfig = ''
+      debug
+      email acme@asdrubalini.xyz
+      grace_period 60s
+    '';
+
+    extraConfig = config;
+  };
+
+  systemd.services.caddy.serviceConfig.EnvironmentFile = [ "/persist/caddy/env" ];
+}
