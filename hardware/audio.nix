fbeb3422d8537abb42fb674745eeb28e530d0719
Irene <git@irene.foo>
2026-04-15 10:15:44 +0100

    fix bluetooth

--- hardware/audio.nix
+++ hardware/audio.nix
@@ -6,5 +6,13 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    wireplumber.extraConfig."11-bluetooth-policy" = {
+      "wireplumber.settings" = {
+        "bluetooth.autoswitch-to-headset-profile" = false;
+      };
+      "monitor.bluez.properties" = {
+        "bluez5.codecs" = [ "sbc" "sbc_xq" "aac" "aptx" "aptx_hd" ];
+      };
+    };
   };
 }
