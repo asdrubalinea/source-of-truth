37da10918581177474de92419b13ad6261467faf
Irene <git@irene.foo>
2026-08-22 23:43:42 +0000

    update caddy

--- services/caddy/default.nix
+++ services/caddy/default.nix
@@ -14,7 +14,7 @@ in
 
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
-      hash = "sha256-Gb1nC5fZfj7IodQmKmEPGygIHNYhKWV1L0JJiqnVtbs=";
+      hash = "sha256-qEA6058svI8Q6yE97OkfnGWC8ayI3x8y2iU7PGkJ3Do=";
     };
 
     globalConfig = ''
