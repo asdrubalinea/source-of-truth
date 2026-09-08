4fc277729d98a5ab393fbe982e5701dfbf7de014
Irene <git@irene.foo>
2026-09-08 23:38:31 +0100

    caddy: reformat the plugin list

--- services/caddy/default.nix
+++ services/caddy/default.nix
@@ -20,7 +20,7 @@ in {
     enable = true;
 
     package = pkgs.caddy.withPlugins {
-      plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
+      plugins = ["github.com/caddy-dns/cloudflare@v0.2.2"];
       hash = "sha256-qEA6058svI8Q6yE97OkfnGWC8ayI3x8y2iU7PGkJ3Do=";
     };
 
